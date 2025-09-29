168d4326de476a7a6609778646cbe9de1e933d17
EugeniaMolinari1 <eugeniamolinaari@gmail.com>
2025-09-29 11:35:57 -0300

    entrega tp4

---
+++ TP4-Lista/datos_jedi.py
@@ -0,0 +1,72 @@
+JEDI_DATA = [
+    {
+        "nombre": "Yoda",
+        "maestros": [],
+        "padawans": ["Luke Skywalker", "Count Dooku"],
+        "colores_sable": ["verde"],
+        "especie": "desconocida"
+    },
+    {
+        "nombre": "Luke Skywalker",
+        "maestros": ["Yoda", "Obi-Wan Kenobi"],
+        "padawans": ["Grogu"],
+        "colores_sable": ["verde", "azul"],
+        "especie": "humano"
+    },
+    {
+        "nombre": "Ahsoka Tano",
+        "maestros": ["Anakin Skywalker"],
+        "padawans": [],
+        "colores_sable": ["verde", "blanco"],
+        "especie": "togruta"
+    },
+    {
+        "nombre": "Kit Fisto",
+        "maestros": [],
+        "padawans": [],
+        "colores_sable": ["verde"],
+        "especie": "nautolano"
+    },
+    {
+        "nombre": "Mace Windu",
+        "maestros": [],
+        "padawans": [],
+        "colores_sable": ["violeta"],
+        "especie": "humano"
+    },
+    {
+        "nombre": "Qui-Gon Jin",
+        "maestros": ["Count Dooku"],
+        "padawans": ["Obi-Wan Kenobi"],
+        "colores_sable": ["verde"],
+        "especie": "humano"
+    },
+    {
+        "nombre": "Obi-Wan Kenobi",
+        "maestros": ["Qui-Gon Jin"],
+        "padawans": ["Anakin Skywalker"],
+        "colores_sable": ["azul"],
+        "especie": "humano"
+    },
+    {
+        "nombre": "Anakin Skywalker",
+        "maestros": ["Obi-Wan Kenobi"],
+        "padawans": ["Ahsoka Tano"],
+        "colores_sable": ["azul", "rojo"],
+        "especie": "humano"
+    },
+    {
+        "nombre": "Plo Koon",
+        "maestros": [],
+        "padawans": [],
+        "colores_sable": ["naranja"],
+        "especie": "kel dor"
+    },
+    {
+        "nombre": "Aayla Secura",
+        "maestros": ["Quinlan Vos"],
+        "padawans": [],
+        "colores_sable": ["azul"],
+        "especie": "twi'lek"
+    }
+]
---
+++ TP4-Lista/listanueva_superheroes.py
@@ -0,0 +1,14 @@
+# superheroes_listanueva.py
+
+superheroes_nueva = [
+    {"nombre": "Linterna Verde", "anio": 1940, "casa": "DC", "biografia": "Portador de un anillo de poder y traje del Cuerpo de Linternas."},
+    {"nombre": "Wolverine", "anio": 1974, "casa": "Marvel", "biografia": "Mutante con garras de adamantium y factor de curación."},
+    {"nombre": "Dr. Strange", "anio": 1963, "casa": "DC", "biografia": "Hechicero maestro de las artes místicas."},
+    {"nombre": "Capitana Marvel", "anio": 1967, "casa": "Marvel", "biografia": "Piloto con energía cósmica; usa traje de combate."},
+    {"nombre": "Mujer Maravilla", "anio": 1941, "casa": "DC", "biografia": "Guerrera amazona con lazo de la verdad."},
+    {"nombre": "Flash", "anio": 1940, "casa": "DC", "biografia": "El hombre más rápido; usa traje aerodinámico."},
+    {"nombre": "Star-Lord", "anio": 1976, "casa": "Marvel", "biografia": "Líder de los Guardianes; usa un casco y armadura ligera."},
+    {"nombre": "Batman", "anio": 1939, "casa": "DC", "biografia": "Detective con traje y gadgets; protector de Gotham."},
+    {"nombre": "Iron Man", "anio": 1963, "casa": "Marvel", "biografia": "Tony Stark combate con una armadura de alta tecnología."},
+    {"nombre": "Superman", "anio": 1938, "casa": "DC", "biografia": "Kryptoniano con capa y traje icónico."},
+]
